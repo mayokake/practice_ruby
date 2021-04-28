0aeb5bca20568d10d42e6f788b6ce4738991b7c8
mayokake <masataka.ikeda@mayokake.com>
2021-04-28 22:17:06 +0900

    練習２を解答した

---
+++ beginner_ruby/practice2.rb
@@ -0,0 +1,27 @@
+puts 2 + 3 #2-1
+puts 2 * 2 * 3.14
+
+puts "Ruby"
+puts "abc" + "def"
+puts "123".to_i + "456".to_i
+
+coffee = 300
+espresso = 100
+puts "コーヒー:#{coffee}円"
+puts "合計:#{coffee + espresso * 2}"
+
+coffee = 400
+espresso = 100
+puts "コーヒー:#{coffee}円"
+puts "合計: #{coffee + espresso * 2}円"
+
+# 2-6
+total = 300 * 2
+p total
+tax = total * 0.08
+puts tax
+puts total + tax
+
+#2-7
+total = 300 * 2
+puts total
