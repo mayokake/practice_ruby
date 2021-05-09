5e45f037bc7e2ea7d70b4d47a521cc24fb17dc6a
mayokake <masataka.ikeda@mayokake.com>
2021-05-09 15:30:09 +0900

    RGBテスト完了

--- ruby_for_pro/lib/rgb.rb
+++ ruby_for_pro/lib/rgb.rb
@@ -1,3 +1,15 @@
 def to_hex(r, g, b)
-  "#000000"
+  [r, g, b].inject("#") do |hex, n|
+    hex + n.to_s(16).rjust(2, "0")
+  end
+end
+
+def to_ints(hex)
+  r = hex[1..2]
+  g = hex[3..4]
+  b = hex[5..6]
+  ints = []
+  [r, g, b].map do |s|
+    s.hex
+  end
 end
