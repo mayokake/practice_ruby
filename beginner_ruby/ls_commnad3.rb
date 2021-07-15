3d16972fa8d3d634f57648a4ea72afb1e0f8b03f
mayokake <masataka.ikeda@mayokake.com>
2021-07-16 01:49:50 +0900

    大幅修正クラス化その２

--- beginner_ruby/ls_commnad3.rb
+++ beginner_ruby/ls_commnad3.rb
@@ -31,324 +31,494 @@ class ArrayForMatrix
   end
 end
 
-class MatrixForNoL
-  def initialize(array)
-    @array = array
+# class MatrixForNoL
+#   def initialize(array)
+#     @array = array
+#   end
+
+#   def number_of_columns
+#     3
+#   end
+
+#   def length_of_array
+#     @array.size
+#   end
+
+#   def width_of_row
+#     @array.map(&:size).max > 25 ? @array.map(&:size).max : 25
+#   end
+
+#   def length_of_row
+#     if (length_of_array.divmod(number_of_columns)[1]).zero?
+#       length_of_array.divmod(number_of_columns)[0]
+#     else
+#       length_of_array.divmod(number_of_columns)[0] + 1
+#     end
+#   end
+
+#   def number_of_adding_strings
+#     if length_of_array.divmod(number_of_columns)[1] != 0
+#       length_of_row - length_of_array.divmod(length_of_row)[1]
+#     else
+#       0
+#     end
+#   end
+  
+#   def divisible_array
+#     @array + Array.new(number_of_adding_strings, '')
+#   end
+
+#   def size_ajustment
+#     divisible_array.map { |string| string.ljust(width_of_row)}
+#   end
+
+#   def array_for_transpose
+#     three_dimension_array = []
+#     size_ajustment.each_slice(length_of_row) { |string| three_dimension_array << string }
+#     three_dimension_array
+#   end
+
+#   def transposed_array
+#     array_for_transpose.transpose
+#   end
+# end
+
+# test = ArrayForMatrix.new
+# test1 = test.array_for_ar_option
+# test00 = test.l_option
+
+# test2 = MatrixForNoL.new(test1)
+# test10 = test2.transposed_array
+
+# def output_display(array)
+#   array.each do |file| 
+#     file.each.with_index do |elemental, index|
+#       if file.size == index + 1
+#         print "#{elemental}\n"
+#       else
+#         print elemental
+#       end
+#     end
+#   end
+# end
+
+# output_display(test10) if test00 == false
+
+# test = ArrayForMatrix.new
+# test01 = test.array_for_stat
+
+
+
+# @parameter = ARGV.getopts('lar')
+
+class MatrixForL
+  def initialize(array1, array2)
+    @array1 = array1
+    @array2 = array2
   end
 
-  def number_of_columns
-    3
+  def file_type(file)
+    hash1 = { 'file' => '-', 'directory' => 'd', 'characterSpecial' => 'c', 'blockSpecial' => 'b',
+              'fifo' => 'p', 'link' => 'l', 'socket' => 's', 'unknown' => '?' }
+    hash1[file.ftype]
   end
 
-  def length_of_array
-    @array.size
+  def rwx(number)
+    hash2 = { '7' => 'rwx', '6' => 'rw-', '5' => 'r-x', '4' => 'r--', '3' => '-wx', '2' => '-w-', '1' => '--x',
+              '0' => '---' }
+    hash2[number]
   end
-
-  def width_of_row
-    @array.map(&:size).max > 25 ? @array.map(&:size).max : 25
+  
+  def id(number)
+    hash3 = { '7' => 'rws', '6' => 'rwS', '5' => 'r-s', '4' => 'r-S', '3' => '-ws', '2' => '-wS', '1' => '--s',
+              '0' => '--S' }
+    hash3[number]
   end
-
-  def length_of_row
-    if (length_of_array.divmod(number_of_columns)[1]).zero?
-      length_of_array.divmod(number_of_columns)[0]
+  
+  def file_mode_owner(file)
+    local_mode = file.mode.to_s(8).slice(-3)
+    id_mode = file.mode.to_s(8).slice(-4)
+    if id_mode == '4'
+      id(local_mode)
     else
-      length_of_array.divmod(number_of_columns)[0] + 1
-    end
-  end
-
-  def number_of_adding_strings
-    if length_of_array.divmod(number_of_columns)[1] != 0
-      length_of_row - length_of_array.divmod(length_of_row)[1]
-    else
-      0
+      rwx(local_mode)
     end
   end
   
-  def divisible_array
-    @array + Array.new(number_of_adding_strings, '')
+  def file_mode_group(file1)
+    local_mode = file1.mode.to_s(8).slice(-2)
+    id_mode = file1.mode.to_s(8).slice(-4)
+    if id_mode == '2'
+      id(local_mode)
+    else
+      rwx(local_mode)
+    end
+  end
+  
+  def file_mode_other(file1)
+    local_mode = file1.mode.to_s(8).slice(-1)
+    sticky_mode = file1.mode.to_s(8).slice(-4)
+    if local_mode == '4' && sticky_mode == '1'
+      'r-T'
+    elsif local_mode == '5' && sticky_mode == '1'
+      'r-t'
+    else
+      rwx(local_mode)
+    end
+  end
+  
+  def file_permission(file1)
+    arr_atribute = []
+    arr_atribute << file_type(file1)
+    arr_atribute << file_mode_owner(file1)
+    arr_atribute << file_mode_group(file1)
+    arr_atribute << file_mode_other(file1)
+    arr_atribute.join.ljust(11)
+  end
+  
+  def file_and_permission
+    @array1.map do |file|
+      file_permission(file)
+    end
+  end
+  
+  def number_of_links
+    @array1.map do |file|
+      file.nlink.to_s.rjust(4)
+    end
+  end
+  
+  def uid
+    @array1.map do |file|
+      Etc.getpwuid(file.uid).name.rjust(6)
+    end
+  end
+  
+  def gid
+    @array1.map do |file|
+      Etc.getgrgid(file.gid).name.rjust(6)
+    end
+  end
+  
+  def size
+    @array1.map do |file|
+      file.size.to_s.rjust(9)
+    end
+  end
+  
+  def year(file)
+    file.mtime.year.to_s.rjust(5)
+  end
+  
+  def month(file)
+    file.mtime.month.to_s.rjust(2)
+  end
+  
+  def day(file)
+    file.mtime.day.to_s.rjust(2)
+  end
+  
+  def time_ob(file)
+    file.mtime.strftime('%H:%M')
+  end
+  
+  def date
+    @array1.map do |file|
+      if ((Time.now - file.mtime) / 60 / 60 / 24).round > 365 / 2
+        "#{month(file)} #{day(file)} #{year(file)}"
+      else
+        "#{month(file)} #{day(file)} #{time_ob(file)}"
+      end
+    end
   end
 
-  def size_ajustment
-    divisible_array.map { |string| string.ljust(width_of_row)}
+  def symlink
+    @array2.map.with_index do |file, index|
+      if @array1[index].symlink?
+        "-> #{File.readlink(file)}"
+      else
+        ''
+      end
+    end
+  end
+  
+  def blocks_number
+    @array1.map(&:blocks)
+  end
+  
+  def matrix
+    matrix = []
+    matrix << file_and_permission
+    matrix << number_of_links
+    matrix << uid
+    matrix << gid
+    matrix << size
+    matrix << date
+    matrix << @array2
+    matrix << symlink
   end
 
-  def array_for_transpose
-    three_dimension_array = []
-    size_ajustment.each_slice(length_of_row) { |string| three_dimension_array << string }
-    three_dimension_array
-  end
-
-  def transposed_array
-    array_for_transpose.transpose
+  def transposed_matrix
+    matrix.transpose
   end
 end
 
 
-# # Matrixを作るところはクラス化したい
-
 test = ArrayForMatrix.new
-test1 = test.array_for_ar_option
+test01 = test.array_for_stat
+test02 = test.array_for_ar_option
 test00 = test.l_option
 
-test2 = MatrixForNoL.new(test1)
-# p test3 = test2.number_of_columns
-# p test4 = test2.length_of_array
-# p test5 = test2.width_of_row
-# p test6 = test2.length_of_row
-# p test7 = test2.number_of_adding_strings
-# p test8 = test2.warikireru_array
-# p test0 = test2.size_ajustment
-# p test9 = test2.array_for_transpose
-test10 = test2.transposed_array
+test2 = MatrixForL.new(test01, test02)
+test20 = test2.transposed_matrix
 
-def output_display(array)
-  array.each do |file| 
+def another_display(mtx)
+  mtx.each do |file|
     file.each.with_index do |elemental, index|
       if file.size == index + 1
-        print "#{elemental}\n"
+        print "#{elemental} \n"
       else
-        print elemental
+        print "#{elemental} "
       end
     end
   end
 end
 
+if test00 == true
+  puts "total #{test2.blocks_number.sum}"
+  another_display(test20)
+end
 
-output_display(test10) if test00 == false
 
 
-# p test.array_for_stat
+# # def array_for_a_option
+# #   @parameter['a'] ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*')
+# # end
 
-# @parameter = ARGV.getopts('lar')
+# # def array_for_ar_option
+# #   @parameter['r'] ? array_for_a_option.reverse : array_for_a_option
+# # end
 
-# def array_for_a_option
-#   @parameter['a'] ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*')
-# end
-
-# def array_for_ar_option
-#   @parameter['r'] ? array_for_a_option.reverse : array_for_a_option
-# end
-
-# def array_for_stat
-#   array_for_ar_option.map do |file|
-#     File.lstat(file).ftype == 'link' ? File.lstat(file) : File.stat(file)
-#   end
-# end
-
+# # def array_for_stat
+# #   array_for_ar_option.map do |file|
+# #     File.lstat(file).ftype == 'link' ? File.lstat(file) : File.stat(file)
+# #   end
+# # end
 
 
 
 
-# def number_of_columns # done
-#   3
-# end
 
-# def length_of_array # done
-#   array_for_a_option.size
-# end
+# # def number_of_columns # done
+# #   3
+# # end
 
-# def width_of_row # done
-#   array_for_a_option.map(&:size).max > 25 ? array_for_a_option.map(&:size).max : 25
-# end
+# # def length_of_array # done
+# #   array_for_a_option.size
+# # end
 
-# def length_of_row # done
-#   if (length_of_array.divmod(number_of_columns)[1]).zero?
-#     length_of_array.divmod(number_of_columns)[0]
-#   else
-#     length_of_array.divmod(number_of_columns)[0] + 1
-#   end
-# end
+# # def width_of_row # done
+# #   array_for_a_option.map(&:size).max > 25 ? array_for_a_option.map(&:size).max : 25
+# # end
 
-# def for_add_strings #done
-#   if length_of_array.divmod(number_of_columns)[1] != 0
-#     length_of_row - length_of_array.divmod(length_of_row)[1]
-#   else
-#     0
-#   end
-# end
+# # def length_of_row # done
+# #   if (length_of_array.divmod(number_of_columns)[1]).zero?
+# #     length_of_array.divmod(number_of_columns)[0]
+# #   else
+# #     length_of_array.divmod(number_of_columns)[0] + 1
+# #   end
+# # end
 
-# def array_for_transpose # done
-#   ary = array_for_ar_option + Array.new(for_add_strings, '')
-#   three_dimension_array = []
-#   ary.each_slice(length_of_row) { |s| three_dimension_array << s }
-#   three_dimension_array
-# end
+# # def for_add_strings #done
+# #   if length_of_array.divmod(number_of_columns)[1] != 0
+# #     length_of_row - length_of_array.divmod(length_of_row)[1]
+# #   else
+# #     0
+# #   end
+# # end
+
+# # def array_for_transpose # done
+# #   ary = array_for_ar_option + Array.new(for_add_strings, '')
+# #   three_dimension_array = []
+# #   ary.each_slice(length_of_row) { |s| three_dimension_array << s }
+# #   three_dimension_array
+# # end
 
 
 
-# def output_display(array)
-#   array.transpose.each do |file| 
-#     # transposeは外に出す
-#     file.each.with_index do |elemental, index|
-#       if file.size == index + 1 # index == number_of_columns - 1
-#         # print "#{elemental.ljust(width_of_row)}\n"
-#         print "#{elemental.ljust(45)}\n"
-#       else
-#         # print elemental.ljust(width_of_row)
-#         print elemental.ljust(45)
-#       end
-#     end
-#   end
-# end
+# # def output_display(array)
+# #   array.transpose.each do |file| 
+# #     # transposeは外に出す
+# #     file.each.with_index do |elemental, index|
+# #       if file.size == index + 1 # index == number_of_columns - 1
+# #         # print "#{elemental.ljust(width_of_row)}\n"
+# #         print "#{elemental.ljust(45)}\n"
+# #       else
+# #         # print elemental.ljust(width_of_row)
+# #         print elemental.ljust(45)
+# #       end
+# #     end
+# #   end
+# # end
 
-# output_display(array_for_transpose) if @parameter['l'] == false
+# # output_display(array_for_transpose) if @parameter['l'] == false
 
 # # class
 
-# def file_type(file1)
-#   hash1 = { 'file' => '-', 'directory' => 'd', 'characterSpecial' => 'c', 'blockSpecial' => 'b',
-#             'fifo' => 'p', 'link' => 'l', 'socket' => 's', 'unknown' => '?' }
-#   hash1[file1.ftype]
-# end
+# # def file_type(file1)
+# #   hash1 = { 'file' => '-', 'directory' => 'd', 'characterSpecial' => 'c', 'blockSpecial' => 'b',
+# #             'fifo' => 'p', 'link' => 'l', 'socket' => 's', 'unknown' => '?' }
+# #   hash1[file1.ftype]
+# # end
 
-# def rwx(num_string)
-#   hash2 = { '7' => 'rwx', '6' => 'rw-', '5' => 'r-x', '4' => 'r--', '3' => '-wx', '2' => '-w-', '1' => '--x',
-#             '0' => '---' }
-#   hash2[num_string]
-# end
+# # def rwx(num_string)
+# #   hash2 = { '7' => 'rwx', '6' => 'rw-', '5' => 'r-x', '4' => 'r--', '3' => '-wx', '2' => '-w-', '1' => '--x',
+# #             '0' => '---' }
+# #   hash2[num_string]
+# # end
 
-# def id(num_string2)
-#   hash3 = { '7' => 'rws', '6' => 'rwS', '5' => 'r-s', '4' => 'r-S', '3' => '-ws', '2' => '-wS', '1' => '--s',
-#             '0' => '--S' }
-#   hash3[num_string2]
-# end
+# # def id(num_string2)
+# #   hash3 = { '7' => 'rws', '6' => 'rwS', '5' => 'r-s', '4' => 'r-S', '3' => '-ws', '2' => '-wS', '1' => '--s',
+# #             '0' => '--S' }
+# #   hash3[num_string2]
+# # end
 
-# def file_mode_owner(file1)
-#   local_mode = file1.mode.to_s(8).slice(-3)
-#   id_mode = file1.mode.to_s(8).slice(-4)
-#   if id_mode == '4'
-#     id(local_mode)
-#   else
-#     rwx(local_mode)
-#   end
-# end
+# # def file_mode_owner(file1)
+# #   local_mode = file1.mode.to_s(8).slice(-3)
+# #   id_mode = file1.mode.to_s(8).slice(-4)
+# #   if id_mode == '4'
+# #     id(local_mode)
+# #   else
+# #     rwx(local_mode)
+# #   end
+# # end
 
-# def file_mode_group(file1)
-#   local_mode = file1.mode.to_s(8).slice(-2)
-#   id_mode = file1.mode.to_s(8).slice(-4)
-#   if id_mode == '2'
-#     id(local_mode)
-#   else
-#     rwx(local_mode)
-#   end
-# end
+# # def file_mode_group(file1)
+# #   local_mode = file1.mode.to_s(8).slice(-2)
+# #   id_mode = file1.mode.to_s(8).slice(-4)
+# #   if id_mode == '2'
+# #     id(local_mode)
+# #   else
+# #     rwx(local_mode)
+# #   end
+# # end
 
-# def file_mode_other(file1)
-#   local_mode = file1.mode.to_s(8).slice(-1)
-#   sticky_mode = file1.mode.to_s(8).slice(-4)
-#   if local_mode == '4' && sticky_mode == '1'
-#     'r-T'
-#   elsif local_mode == '5' && sticky_mode == '1'
-#     'r-t'
-#   else
-#     rwx(local_mode)
-#   end
-# end
+# # def file_mode_other(file1)
+# #   local_mode = file1.mode.to_s(8).slice(-1)
+# #   sticky_mode = file1.mode.to_s(8).slice(-4)
+# #   if local_mode == '4' && sticky_mode == '1'
+# #     'r-T'
+# #   elsif local_mode == '5' && sticky_mode == '1'
+# #     'r-t'
+# #   else
+# #     rwx(local_mode)
+# #   end
+# # end
 
-# def file_permission(file1)
-#   arr_atribute = []
-#   arr_atribute << file_type(file1)
-#   arr_atribute << file_mode_owner(file1)
-#   arr_atribute << file_mode_group(file1)
-#   arr_atribute << file_mode_other(file1)
-#   arr_atribute.join.ljust(11)
-# end
+# # def file_permission(file1)
+# #   arr_atribute = []
+# #   arr_atribute << file_type(file1)
+# #   arr_atribute << file_mode_owner(file1)
+# #   arr_atribute << file_mode_group(file1)
+# #   arr_atribute << file_mode_other(file1)
+# #   arr_atribute.join.ljust(11)
+# # end
 
-# def file_and_permission
-#   array_for_stat.map do |file|
-#     file_permission(file)
-#   end
-# end
+# # def file_and_permission
+# #   array_for_stat.map do |file|
+# #     file_permission(file)
+# #   end
+# # end
 
-# def number_of_links
-#   array_for_stat.map do |file|
-#     file.nlink.to_s.rjust(4)
-#   end
-# end
+# # def number_of_links
+# #   array_for_stat.map do |file|
+# #     file.nlink.to_s.rjust(4)
+# #   end
+# # end
 
-# def uid
-#   array_for_stat.map do |file|
-#     Etc.getpwuid(file.uid).name.rjust(6)
-#   end
-# end
+# # def uid
+# #   array_for_stat.map do |file|
+# #     Etc.getpwuid(file.uid).name.rjust(6)
+# #   end
+# # end
 
-# def gid
-#   array_for_stat.map do |file|
-#     Etc.getgrgid(file.gid).name.rjust(6)
-#   end
-# end
+# # def gid
+# #   array_for_stat.map do |file|
+# #     Etc.getgrgid(file.gid).name.rjust(6)
+# #   end
+# # end
 
-# def size
-#   array_for_stat.map do |file|
-#     file.size.to_s.rjust(9)
-#   end
-# end
+# # def size
+# #   array_for_stat.map do |file|
+# #     file.size.to_s.rjust(9)
+# #   end
+# # end
 
-# def year(file)
-#   file.mtime.year.to_s.rjust(5)
-# end
+# # def year(file)
+# #   file.mtime.year.to_s.rjust(5)
+# # end
 
-# def month(file)
-#   file.mtime.month.to_s.rjust(2)
-# end
+# # def month(file)
+# #   file.mtime.month.to_s.rjust(2)
+# # end
 
-# def day(file)
-#   file.mtime.day.to_s.rjust(2)
-# end
+# # def day(file)
+# #   file.mtime.day.to_s.rjust(2)
+# # end
 
-# def time_ob(file)
-#   file.mtime.strftime('%H:%M')
-# end
+# # def time_ob(file)
+# #   file.mtime.strftime('%H:%M')
+# # end
 
-# def date
-#   array_for_stat.map do |file|
-#     if ((Time.now - file.mtime) / 60 / 60 / 24).round > 365 / 2
-#       "#{month(file)} #{day(file)} #{year(file)}"
-#     else
-#       "#{month(file)} #{day(file)} #{time_ob(file)}"
-#     end
-#   end
-# end
+# # def date
+# #   array_for_stat.map do |file|
+# #     if ((Time.now - file.mtime) / 60 / 60 / 24).round > 365 / 2
+# #       "#{month(file)} #{day(file)} #{year(file)}"
+# #     else
+# #       "#{month(file)} #{day(file)} #{time_ob(file)}"
+# #     end
+# #   end
+# # end
 
-# def symlink
-#   array_for_ar_option.map.with_index do |file, index|
-#     if array_for_stat[index].symlink?
-#       "-> #{File.readlink(file)}"
-#     else
-#       ''
-#     end
-#   end
-# end
+# # def symlink
+# #   array_for_ar_option.map.with_index do |file, index|
+# #     if array_for_stat[index].symlink?
+# #       "-> #{File.readlink(file)}"
+# #     else
+# #       ''
+# #     end
+# #   end
+# # end
 
-# def blocks_number
-#   array_for_stat.map(&:blocks)
-# end
+# # def blocks_number
+# #   array_for_stat.map(&:blocks)
+# # end
 
-# def matrix
-#   matrix = []
-#   matrix << file_and_permission
-#   matrix << number_of_links
-#   matrix << uid
-#   matrix << gid
-#   matrix << size
-#   matrix << date
-#   matrix << array_for_ar_option
-#   matrix << symlink
-# end
+# # def matrix
+# #   matrix = []
+# #   matrix << file_and_permission
+# #   matrix << number_of_links
+# #   matrix << uid
+# #   matrix << gid
+# #   matrix << size
+# #   matrix << date
+# #   matrix << array_for_ar_option
+# #   matrix << symlink
+# # end
 
-# def another_display(mtx)
-#   mtx.transpose.each do |file|
-#     file.each.with_index do |elemental, index|
-#       if file.size == index + 1
-#         print "#{elemental} \n"
-#       else
-#         print "#{elemental} "
-#       end
-#     end
-#   end
-# end
+# # def another_display(mtx)
+# #   mtx.transpose.each do |file|
+# #     file.each.with_index do |elemental, index|
+# #       if file.size == index + 1
+# #         print "#{elemental} \n"
+# #       else
+# #         print "#{elemental} "
+# #       end
+# #     end
+# #   end
+# # end
 
-# if @parameter['l'] == true
-#   puts "total #{blocks_number.sum}"
-#   another_display(matrix)
-# end
+# # if @parameter['l'] == true
+# #   puts "total #{blocks_number.sum}"
+# #   another_display(matrix)
+# # end
