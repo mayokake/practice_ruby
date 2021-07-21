a873908bab7bc02ad82f09b62c3770d92bf4a55e
mayokake <masataka.ikeda@mayokake.com>
2021-07-21 13:46:01 +0900

    基本のアレイを修正

--- beginner_ruby/betauchi.rb
+++ beginner_ruby/betauchi.rb
@@ -3,7 +3,7 @@
 require 'etc'
 require 'optparse'
 
-Dir.chdir('/usr/bin')
+# Dir.chdir('/usr/bin')
 # Dir.chdir('/usr/sbin')
 # Dir.chdir("/Users/masataka_ikeda")
 # p Dir.pwd
@@ -13,26 +13,10 @@ parameter = ARGV.getopts('lar')
 l_option = parameter['l']
 
 array_for_a_option = parameter['a'] ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*')
-
 array_for_ar_option = parameter['r'] ? array_for_a_option.reverse : array_for_a_option
 
-array_for_stat = array_for_ar_option.map do |string|
-  File.lstat(string).ftype == 'link' ? File.lstat(string) : File.stat(string)
-end
+array_for_stat = array_for_ar_option.map {|string| File.lstat(string).ftype == 'link' ? File.lstat(string) : File.stat(string) }
 
-# string_uid = array_for_stat.map(&:uid)
-# uid  = string_uid.map do |id|
-#   Etc.getpwuid(id).name
-# end
-# width_of_uid = uid.map(&:size).max
-
-
-# string_gid = array_for_stat.map(&:gid)
-# gid = 
-# string_gid.map do |id|
-#   Etc.getgrgid(id).name
-# end
-# width_of_gid = gid.map(&:size).max
 
 # Describing the matrix without the long option
 array_without_l_option = array_for_ar_option
@@ -41,10 +25,10 @@ length_of_array_without_l_option = array_for_ar_option.size
 
 width_of_row_without_l_option = array_for_ar_option.map(&:size).max > 25 ? array_for_ar_option.map(&:size).max : 25
 
-p condition_length = length_of_array_without_l_option.divmod(number_of_columns)
-p condition_length[1].zero?
-p length_of_row = condition_length[1].zero? ? condition_length[0] : condition_length[0] + 1
-p number_of_adding_strings = condition_length[1].zero? ? 0 : length_of_row - length_of_array_without_l_option.divmod(length_of_row)[1]
+condition_length = length_of_array_without_l_option.divmod(number_of_columns)
+condition_length[1].zero?
+length_of_row = condition_length[1].zero? ? condition_length[0] : condition_length[0] + 1
+number_of_adding_strings = condition_length[1].zero? ? 0 : length_of_row - length_of_array_without_l_option.divmod(length_of_row)[1]
 divisible_array = array_without_l_option + Array.new(number_of_adding_strings, '')
 size_ajustment = divisible_array.map { |string| string.ljust(width_of_row_without_l_option) }
 matrix_for_transpose = size_ajustment.each_slice(length_of_row).to_a
@@ -63,67 +47,9 @@ def output_without_l_option(array)
   end
 end
 
-p array_without_l_option
+# p array_without_l_option
 
-output_without_l_option(transposed_matrix_without_l_option)
-
-
-
-# Describing the matrix without the long option
-# class MtxForNoL
-  # def initialize(array, number)
-  #   @array = array
-  #   @number_of_columns = number
-  # end
-
-  # def length_of_array
-  #   test = @array.size
-  #   @length_of_array = test
-  # end
-
-  # for adjustment of the width of file names displayed
-  # def width_of_row
-  #   @array.map(&:size).max > 25 ? @array.map(&:size).max : 25
-  # end
-
-  # def length_of_row
-  #   # debug_with_sleep(length_of_row)
-  #   length_of_array
-  #   if (@length_of_array.divmod(@number_of_columns)[1]).zero?
-  #     @length_of_array.divmod(@number_of_columns)[0]
-  #   else
-  #     @length_of_array.divmod(@number_of_columns)[0] + 1
-  #   end
-  # end
-
-  # def number_of_adding_strings
-  #   if length_of_array.divmod(@number_of_columns)[1] != 0
-  #     length_of_row - @length_of_array.divmod(length_of_row)[1]
-  #   else
-  #     0
-  #   end
-  # end
-
-  # def divisible_array
-  #   @array + Array.new(number_of_adding_strings, '')
-  # end
-
-  # def size_ajustment
-  #   width_of_row
-  #   divisible_array.map { |string| string.ljust(width_of_row) }
-  # end
-
-  # def matrix_for_transpose
-  #   three_dimension_array = []
-  #   size_ajustment.each_slice(length_of_row) { |string| three_dimension_array << string }
-  #   three_dimension_array
-  # end
-
-#   def transposed_matrix_without_l_option
-#     matrix_for_transpose.transpose
-#   end
-# end
-
+# ##### output_without_l_option(transposed_matrix_without_l_option)
 
 # file permission
 class ModeAndPermission
@@ -206,25 +132,21 @@ end
 
 ModeAndPermission.my_file_permission(array_for_stat)
 
-number_of_links = array_for_stat.map { |stat| stat.nlink.to_s.rjust(4) }
+def links_uid_gid_size(array)
+  links = array.map { |stat| stat.nlink.to_s.rjust(4) }
+  width_of_uid = array.map { |stat| Etc.getpwuid(stat.uid).name.size }.max
+  uid =  array.map { |stat| Etc.getpwuid(stat.uid).name.rjust(width_of_uid) }
+  width_of_gid = array.map { |stat| Etc.getgrgid(stat.gid).name.size }.max
+  gid = array.map { |stat| Etc.getgrgid(stat.gid).name.rjust(width_of_gid) }
+  size = array.map {|stat| stat.size.to_s.rjust(9) }
+  mini_matrix = []
+  mini_matrix << links
+  mini_matrix << uid
+  mini_matrix << gid
+  mini_matrix << size    
+end
 
-
-width_of_uid = array_for_stat.map { |stat| Etc.getpwuid(stat.uid).name.size }.max
-uid = array_for_stat.map { |stat| Etc.getpwuid(stat.uid).name.rjust(width_of_uid) }
-
-width_of_gid = array_for_stat.map { |stat| Etc.getgrgid(stat.gid).name.size }.max
-gid = array_for_stat.map { |stat| Etc.getgrgid(stat.gid).name.rjust(width_of_gid) }
-
-size = array_for_stat.map {|stat| stat.size.to_s.rjust(9) }
-
-# def display_date(stat)
-#   year = stat.mtime.year.to_s.rjust(5)
-#   month = stat.mtime.month.to_s.rjust(2)
-#   day = stat.mtime.day.to_s.rjust(2)
-#   time_object = stat.mtime.strftime('%H:%M')
-# end
-
-# p display_date(array_for_stat[0])
+p links_uid_gid_size(array_for_stat).size
 
 class DateClassObj
   def self.date_object(array)
@@ -265,37 +187,31 @@ class DateClassObj
   end
 end
 
-DateClassObj.date_object(array_for_stat)
-
-# symbolic_link =
-#   @array2.map.with_index do |string, index|
-#     if @array1[index].symlink?
-#       "-> #{File.readlink(string)}"
-#     else
-#       ''
-#     end
-#   end
-# end
+# DateClassObj.date_object(array_for_stat)
 
 symbolic_link =
 array_for_ar_option.map.with_index do |string, index|
   array_for_stat[index].symlink? ? "-> #{File.readlink(string)}" : ''
 end
 
-p blocks_number = array_for_stat.map(&:blocks)
+# p blocks_number = array_for_stat.map(&:blocks)
+
+p ModeAndPermission.my_file_permission(array_for_stat).size
+p links_uid_gid_size(array_for_stat).size
+p DateClassObj.date_object(array_for_stat).size
+p array_for_ar_option.size
+p symbolic_link.size
+
 
 matrix = []
 matrix << ModeAndPermission.my_file_permission(array_for_stat)
-matrix << number_of_links
-matrix << uid
-matrix << gid
-matrix << size
+matrix = matrix + links_uid_gid_size(array_for_stat)
 matrix << DateClassObj.date_object(array_for_stat)
 matrix << array_for_ar_option
 matrix << symbolic_link
 matrix
 
-transposed_matrix = matrix.transpose
+p transposed_matrix = matrix.transpose
 
 def output_with_l_option(mtx)
   mtx.each do |file|
@@ -311,262 +227,6 @@ end
 
 output_with_l_option(transposed_matrix)
 
-
-# class MtxForL
-#   def initialize(array1, array2, array3, array4, array5)
-#     @array1 = array1
-#     @array2 = array2
-#     @array3 = array3
-#     @array4 = array4
-#     @array5 = array5
-#   end
-
-  # def number_of_links
-  #   @array1.map do |stat|
-  #     stat.nlink.to_s.rjust(4)
-  #   end
-  # end
-
-  # def string_uid
-  #   @array1.map(&:uid)
-  # end
-
-  # def uid
-  #   @array1.map do |stat|
-  #     Etc.getpwuid(stat.uid).name.rjust(@array4)
-  #   end
-  # end
-
-  # def gid
-  #   @array1.map do |stat|
-  #     Etc.getgrgid(stat.gid).name.rjust(@array5)
-  #   end
-  # end
-
-  # def size
-  #   @array1.map do |stat|
-  #     stat.size.to_s.rjust(9)
-  #   end
-  # end
-
-  # def year(stat)
-  #   stat.mtime.year.to_s.rjust(5)
-  # end
-
-  # def month(stat)
-  #   stat.mtime.month.to_s.rjust(2)
-  # end
-
-  # def day(stat)
-  #   stat.mtime.day.to_s.rjust(2)
-  # end
-
-  # def time_ob(stat)
-  #   stat.mtime.strftime('%H:%M')
-  # end
-
-  # def date
-  #   @array1.map do |stat|
-  #     if ((Time.now - stat.mtime) / 60 / 60 / 24).round > 365 / 2
-  #       "#{month(stat)} #{day(stat)} #{year(stat)}"
-  #     else
-  #       "#{month(stat)} #{day(stat)} #{time_ob(stat)}"
-  #     end
-  #   end
-  # end
-
-# symbolic_link
-#     array_for_ar_option.map.with_index do |string, index|
-#       if array_for_stat[index].symlink?
-#         "-> #{File.readlink(string)}"
-#       else
-#         ''
-#       end
-#     end
-#   end
-
-#   def blocks_number
-#     @array1.map(&:blocks)
-#   end
-
-#   def matrix
-#     matrix = []
-#     matrix << @array3
-#     matrix << number_of_links
-#     matrix << uid
-#     matrix << gid
-#     matrix << size
-#     matrix << date
-#     matrix << @array2
-#     matrix << symbolic_link
-#   end
-
-#   def transposed_matrix
-#     matrix.transpose
-#   end
-# end
-
-# def output_without_l_option(array)
-#   array.each do |file|
-#     file.each.with_index do |elemental, index|
-#       if file.size == index + 1
-#         print "#{elemental}\n"
-#       else
-#         print elemental
-#       end
-#     end
-#   end
-# end
-
-# def output_with_l_option(mtx)
-#   mtx.each do |file|
-#     file.each.with_index do |elemental, index|
-#       if file.size == index + 1
-#         print "#{elemental} \n"
-#       else
-#         print "#{elemental} "
-#       end
-#     end
-#   end
-# end
-
-# Describing the matrix with the long option
-# class MtxForL
-#   def initialize(array1, array2, array3, array4, array5)
-#     @array1 = array1
-#     @array2 = array2
-#     @array3 = array3
-#     @array4 = array4
-#     @array5 = array5
-#   end
-
-#   def number_of_links
-#     @array1.map do |stat|
-#       stat.nlink.to_s.rjust(4)
-#     end
-#   end
-
-#   def string_uid
-#     @array1.map(&:uid)
-#   end
-
-#   def uid
-#     @array1.map do |stat|
-#       Etc.getpwuid(stat.uid).name.rjust(@array4)
-#     end
-#   end
-
-#   def gid
-#     @array1.map do |stat|
-#       Etc.getgrgid(stat.gid).name.rjust(@array5)
-#     end
-#   end
-
-#   def size
-#     @array1.map do |stat|
-#       stat.size.to_s.rjust(9)
-#     end
-#   end
-
-#   def year(stat)
-#     stat.mtime.year.to_s.rjust(5)
-#   end
-
-#   def month(stat)
-#     stat.mtime.month.to_s.rjust(2)
-#   end
-
-#   def day(stat)
-#     stat.mtime.day.to_s.rjust(2)
-#   end
-
-#   def time_ob(stat)
-#     stat.mtime.strftime('%H:%M')
-#   end
-
-#   def date
-#     @array1.map do |stat|
-#       if ((Time.now - stat.mtime) / 60 / 60 / 24).round > 365 / 2
-#         "#{month(stat)} #{day(stat)} #{year(stat)}"
-#       else
-#         "#{month(stat)} #{day(stat)} #{time_ob(stat)}"
-#       end
-#     end
-#   end
-
-#   def symbolic_link
-#     @array2.map.with_index do |string, index|
-#       if @array1[index].symlink?
-#         "-> #{File.readlink(string)}"
-#       else
-#         ''
-#       end
-#     end
-#   end
-
-#   def blocks_number
-#     @array1.map(&:blocks)
-#   end
-
-#   def matrix
-#     matrix = []
-#     matrix << @array3
-#     matrix << number_of_links
-#     matrix << uid
-#     matrix << gid
-#     matrix << size
-#     matrix << date
-#     matrix << @array2
-#     matrix << symbolic_link
-#   end
-
-#   def transposed_matrix
-#     matrix.transpose
-#   end
-# end
-
-# def output_without_l_option(array)
-#   array.each do |file|
-#     file.each.with_index do |elemental, index|
-#       if file.size == index + 1
-#         print "#{elemental}\n"
-#       else
-#         print elemental
-#       end
-#     end
-#   end
-# end
-
-# def output_with_l_option(mtx)
-#   mtx.each do |file|
-#     file.each.with_index do |elemental, index|
-#       if file.size == index + 1
-#         print "#{elemental} \n"
-#       else
-#         print "#{elemental} "
-#       end
-#     end
-#   end
-# end
-
-# basic_array = ArrayForMatrix.new
-
-# for_ar_option = basic_array.array_for_ar_option
-# for_statlink = basic_array.array_for_stat
-
-# condition = basic_array.l_option
-
-# width_uid = basic_array.width_of_uid
-# width_gid = basic_array.width_of_gid
-
-# array_with_no_l_option = MtxForNoL.new(for_ar_option)
-# matrix1 = array_with_no_l_option.transposed_matrix_without_l_option
-
-# permissions = ModeAndPermission.new(for_statlink)
-# argument_of_permissions = permissions.file_and_permission
-
-# array_with_l_option = MtxForL.new(for_statlink, for_ar_option, argument_of_permissions, width_uid, width_gid)
-# matrix2 = array_with_l_option.transposed_matrix
 
 # if condition == true
 #   puts "total #{array_with_l_option.blocks_number.sum}"
