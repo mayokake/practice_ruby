152628069ffe604f559bcc436d8aa9f096eb3a3f
mayokake <masataka.ikeda@mayokake.com>
2021-07-29 21:26:29 +0900

    クラス化完了

--- beginner_ruby/wc_command.rb
+++ beginner_ruby/wc_command.rb
@@ -3,160 +3,110 @@
 require 'optparse'
 
 parameter = ARGV.getopts('l')
+l_option = parameter['l']
 array_files_only = Dir.glob('*').filter_map { |string| string if File.stat(string).ftype == 'file' }
 array_from_argument = ARGV.select { |string| array_files_only.include?(string)}
 array_file_read = array_from_argument.map { |string| File.read(string)}
 
-# p array_file_read
-
 class WordCount
+  def self.all(array)
+    word_count = WordCount.new(array)
+    word_count.all
+  end
+
+  def self.total_lines_only(array)
+    word_count2 = WordCount.new(array)
+    word_count2.total_lines_only
+  end
+
+  def self.lines_only(array)
+    word_count3 = WordCount.new(array)
+    word_count3.lines_only
+  end
+
   def initialize(array)
     @array = array
+    @array_convert = @array.map { |string| File.read(string)}
   end
 
+  def all
+    @array_convert.map.with_index do |string, i|
+      merge_data(string)
+      puts " #{file_name(@array[i])}"
+    end
+    total
+  end
+
+  def lines_only
+    @array_convert.map.with_index do |string, i|
+      only_lines(string)
+      puts " #{file_name(@array[i])}"
+    end
+    total_lines_only
+  end
   
+  private
 
-  def file_name(str)
-    str
+  def file_name(string)
+    string
   end
 
-  def lines_number(str)
-    str.count("\n")
+  def lines_number(string)
+    string.count("\n")
   end
 
-  def lines_number_sum(array)
-    numbers = array.map{|string| lines_number(string)}
+  def lines_number_sum
+    numbers = @array_convert.map { |string| lines_number(string) }
     numbers.sum
   end
 
-  def words(str)
-    str.split(/\s/).reject(&:empty?).size
+  def only_lines(input)
+    lines = lines_number(input) # 行数
+    print lines.to_s.rjust(8)
+  end
+
+  def words(string)
+    string.split(/\s/).reject(&:empty?).size
   end
   
-  def words_sum(array)
-    num = array.map{|string| words(string)}
+  def words_sum
+    number = @array_convert.map {|string| words(string)}
+    number.sum
+  end
+
+  def bytes(string)
+    string.bytesize
+  end
+  
+  def bytes_sum
+    num = @array_convert.map{|string| bytes(string)}
     num.sum
   end
 
-  def bytes(str)
-    str.bytesize
-  end
-  
-  def bytes_sum(array)
-    num = array.map{|string| bytes(string)}
-    num.sum
-  end
-
-  def merge_data(input)
-    lines = lines_number(input)
-    words = words(input)
-    bytes = bytes(input)
+  def merge_data(string)
+    lines = lines_number(string)
+    words = words(string)
+    bytes = bytes(string)
     print lines.to_s.rjust(8)
     print words.to_s.rjust(8)
     print bytes.to_s.rjust(8)
-    # puts
   end
 
+  def total
+    lines = lines_number_sum
+    words = words_sum
+    bytes = bytes_sum  
+    print lines.to_s.rjust(8)
+    print words.to_s.rjust(8)
+    print bytes.to_s.rjust(8)
+    puts ' total'
+  end
 
-
-
-
-  
-
-
-
-end
-
-
-def file_name(str)
-  str
-end
-
-# 行数を取得
-def lines_number(str)
-  str.count("\n")
-end
-
-def lines_number_sum(array)
-  num = array.map{|string| lines_number(string)}
-  num.sum
-end
-
-# 単語数を取得
-def words(str)
-  str.split(/\s/).reject(&:empty?).size
-end
-
-def words_sum(array)
-  num = array.map{|string| words(string)}
-  num.sum
-end
-
-# バイト数を取得
-def bytes(str)
-  str.bytesize
-end
-
-def bytes_sum(array)
-  num = array.map{|string| bytes(string)}
-  num.sum
-end
-
-def total(input)
-  lines = lines_number_sum(input) # 行数
-  words = words_sum(input) # 単語数
-  bytes = bytes_sum(input) # バイト数
-
-  print lines.to_s.rjust(8)
-  print words.to_s.rjust(8)
-  print bytes.to_s.rjust(8)
-  puts ' total'
-end
-
-def merge_data(input)
-  lines = lines_number(input) # 行数
-  words = words(input) # 単語数
-  bytes = bytes(input) # バイト数
-
-  print lines.to_s.rjust(8)
-  print words.to_s.rjust(8)
-  print bytes.to_s.rjust(8)
-  # puts
-end
-
-def only_lines(input)
-  lines = lines_number(input) # 行数
-  print lines.to_s.rjust(8)
-end
-
-def total_lines_only(input)
-  lines = lines_number_sum(input) # 行数
-  print lines.to_s.rjust(8)
-  puts ' total'
-end
-
-
-def all(array)
-  array.map do |string|
-    one_string = File.read(string)
-    merge_data(one_string)
-    puts " #{file_name(string)}"
+  def total_lines_only
+    lines = lines_number_sum
+    print lines.to_s.rjust(8)
+    puts ' total'
   end
 end
 
-def lines_only(array)
-  array.map do |string|
-    one_string = File.read(string)
-    only_lines(one_string)
-    puts " #{file_name(string)}"
-  end
-end
-
-lines_only(array_from_argument)
-total_lines_only(array_file_read)
-
-puts ""
-puts ""
-
-all(array_from_argument)
-total(array_file_read)
+l_option ? WordCount.lines_only(array_from_argument) : WordCount.all(array_from_argument)
