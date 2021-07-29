26ab124407c87a860467196c71e81af3d02bfc85
mayokake <masataka.ikeda@mayokake.com>
2021-07-29 15:13:38 +0900

    wcコマンドのベタ打ちが完成

--- beginner_ruby/wc_command.rb
+++ beginner_ruby/wc_command.rb
@@ -2,97 +2,119 @@
 
 require 'optparse'
 
-p ARGV
+# p ARGV
 parameter = ARGV.getopts('l')
-p parameter
 
-# array_for_a_option = Dir.glob('*', File::FNM_DOTMATCH)
-array_for_b_option = Dir.glob('*')
-# array_get = array_for_b_option.map do |string|
-#   string.
-# end
+array_get = Dir.glob('*').filter_map { |string| string if File.stat(string).ftype == 'file' }
+condition_array = ARGV.select { |string| array_get.include?(string)}
+p condition_array
 
-# p File.stat(array_for_b_option[1]).ftype
+string_array = condition_array.map { |string| File.read(string)}
 
-array_get = 
-array_for_b_option.filter_map do |string| 
-  string if File.stat(string).ftype == 'file' 
+def load_name(str)
+  str
 end
 
-p array_get
-# p array_get.select {|string| File.stat(string).ftype == 'file'}
-
-
-
-##### empty?
-
-
-# p array_for_a_option
-# p array_for_b_option
-
-p test1 = ARGV.select { |string| array_get.include?(string)}
-# p test1.size
-# p test2 = ARGV.map {|string| array_for_b_option.include?(string) }
-
-
-
-# if ARGV == []
-#   input = $stdin.read
-# else
-#   puts "$0：#{$0}"
-#   ARGV.each_with_index do |arg, i|
-#   puts "ARGV[#{i}]：#{arg}"
-#   end
-# end
-
-
-# 標準入力を受け取る
-# input = $stdin.read
+# p load_name(condition_array[0])
 
 # 行数を取得
 def load_lines(str)
   str.count("\n")
 end
 
+def load_lines_sum(array)
+  num = array.map{|string| load_lines(string)}
+  num.sum
+end
+
+# p load_lines_sum(string_array)
+
 # 単語数を取得
 def load_words(str)
   str.split(/\s/).reject(&:empty?).size
 end
+
+def load_words_sum(array)
+  num = array.map{|string| load_words(string)}
+  num.sum
+end
+
+# p load_words_sum(string_array)
 
 # バイト数を取得
 def load_bytes(str)
   str.bytesize
 end
 
+def load_bytes_sum(array)
+  num = array.map{|string| load_bytes(string)}
+  num.sum
+end
+
+# p load_bytes_sum(string_array)
+
+def total(input)
+  lines = load_lines_sum(input) # 行数
+  words = load_words_sum(input) # 単語数
+  bytes = load_bytes_sum(input) # バイト数
+
+  print lines.to_s.rjust(8)
+  print words.to_s.rjust(8)
+  print bytes.to_s.rjust(8)
+  puts ' total'
+end
+
+
 def all_data(input)
   lines = load_lines(input) # 行数
   words = load_words(input) # 単語数
   bytes = load_bytes(input) # バイト数
-  
+
   print lines.to_s.rjust(8)
   print words.to_s.rjust(8)
   print bytes.to_s.rjust(8)
-  puts
+  # puts
 end
 
-# all_data(input)
+def only_lines(input)
+  lines = load_lines(input) # 行数
+  print lines.to_s.rjust(8)
+end
+
+def total_lines_only(input)
+  lines = load_lines_sum(input) # 行数
+  print lines.to_s.rjust(8)
+  puts ' total'
+end
 
 
 
+def all(array)
+  array.map do |string|
+    one_string = File.read(string)
+    all_data(one_string)
+    puts " #{load_name(string)}"
+  end
+end
+
+def lines_only(array)
+  array.map do |string|
+    one_string = File.read(string)
+    only_lines(one_string)
+    puts " #{load_name(string)}"
+  end
+end
+
+lines_only(condition_array)
+total_lines_only(string_array)
+
+puts ""
+puts ""
+
+all(condition_array)
+total(string_array)
 
 
-# puts "$0：#{$0}"
-# ARGV.each_with_index do |arg, i|
-#   puts "ARGV[#{i}]：#{arg}"
-# end
-
-# array_for_a_option = Dir.glob('*', File::FNM_DOTMATCH)
-# p array_for_a_option
-# p array_for_a_option.size
-# # p array_for_a_option.include?(ARGV[0])
-# p test1 = ARGV.select { |array| array_for_a_option.include?(array)}
-# p test1.size
-# p test2 = ARGV.map {|array| array_for_a_option.include?(array) }
 
 
 # # ファイル名を取得する、そのファイルがディレクトリに含まれるかどうか確認する必要がある
